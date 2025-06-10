e8e6fece258f591796c4356e30569b0241a51747
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:14:29 +0700

    menunjukan ke sebuah nama file terbuka

---
+++ iofile.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+
+    //operasi file dalam mode nulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file untuk membuka
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
+
+ 
+}
