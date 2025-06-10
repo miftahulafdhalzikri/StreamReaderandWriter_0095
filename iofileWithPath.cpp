9d7f13e99f9e60fe3a6009d543c6e7cbfb116b2d
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:38:18 +0700

    operasi file dalam mode nulis

--- iofileWithPath.cpp
+++ iofileWithPath.cpp
@@ -12,43 +12,17 @@ int main(){
 
     //operasi file dalam mode nulis.
     ofstream outfile;
-    //menunjuk ke sebuah nama file untuk membuka
-    outfile.open(namaFile +".txt");
+  
 
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+      
+     
+      
+  
 
-    //unlimited loop untuk menulis
-    while (true){
-        cout << "-";
-        //mendapatakan setiap karakter dalam satu baris
-        getline(cin, baris);
-        //loop akan berhenti jika anda memasukkan karakter q
-        if (baris == "q") break;
-        // menulis dan memasukan nilai dari 'baris' ke  dalam file
-        outfile << baris << endl;
-    }
-    //selesai dalam menulis sekarang tutup filenya
-    outfile.close();
+  
 
-    //operasi file dalam mode membaca
-    ifstream infile;
-    //menunjuk ke sebuah file untuk membuka
-    infile.open(namaFile +".txt");
 
-    cout << endl << ">= Membuka dan membaca file" << endl;
-    // jika file ada maka
-    if (infile.is_open())
-    {
-        //melakukan perulangan setiap baris
-        while (getline(infile,baris))
-        {
-            //dan tampilka disini
-            cout << baris << '\n';
-        }
-        //tutup file tersebut setelah selesai
-        infile.close();
-    }
-    //jika tidak ditemukan file maka akan menampilkan ini
-    else cout << "unable to open file";
-    return 0;
+ 
+  
+
 }
