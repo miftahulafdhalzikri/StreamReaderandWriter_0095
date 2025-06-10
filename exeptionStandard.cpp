48788da1d5d44731fead791d5c4674c587d87547
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:20:01 +0700

    penangkap menggunakan objek exception

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -13,5 +13,13 @@ int main (){
         cout<<data.at(5)<<endl;
         //memanggil array elemen ke 5
     }
+    catch (exception& e) {
+        //penangkap menggunakan objek exception
+        cout << e.what() << endl;
+        /akan di eksekusi karena array hanya memiliki 3 elemen/
+    }
+    cout << "Baris program yang terakhir" <<endl;
+    /penanda 2: bahwa program berjalantanpa berhenti meskipun terjadi kesalahan/
 
+    return 0;    
 }
