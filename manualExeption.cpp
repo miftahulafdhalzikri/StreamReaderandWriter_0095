61248a849a6b4cc0ca4d04c6f109f988cd4e3892
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:24:46 +0700

    pengecualian akan di eksekusi

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -7,6 +7,9 @@ int main(){
         throw 0,5; //melempar sebuah interger maka
         cout << "pernyataan tidak akan di eksekusi" << endl;
     }
-
+    catch (int a){
+        //blok ini akan di eksekusi
+        cout << "pengecualian akan di eksekusi" << endl;
+    }
    
 }
