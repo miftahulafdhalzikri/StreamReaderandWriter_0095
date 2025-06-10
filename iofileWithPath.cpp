e81e9e2498cf730ff34179ca58232fa70dd32e9c
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:33:42 +0700

    melakukan perulangan setiap baris

--- iofileWithPath.cpp
+++ iofileWithPath.cpp
@@ -38,5 +38,17 @@ int main(){
     cout << endl << ">= Membuka dan membaca file" << endl;
     // jika file ada maka
     if (infile.is_open())
-   
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile,baris))
+        {
+            //dan tampilka disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "unable to open file";
+    return 0;
 }
