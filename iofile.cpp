e0e7c85af4a6a81b7be857116d948b9cad45b035
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:15:09 +0700

    mendapatkan setiap karakter dalam satu baris

--- iofile.cpp
+++ iofile.cpp
@@ -13,7 +13,17 @@ int main(){
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
-
+    //unlimited loop untuk menulis
+    while (true){
+        cout << "-";
+        //mendapatakan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke  dalam file
+        outfile << baris << endl;
+    }
+ 
 
  
 }
