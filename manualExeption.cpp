05f6994fd82ada100e647e6757ad7719133ac652
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:24:21 +0700

    melempar sebuah interger maka

---
+++ manualExeption.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    try {
+        cout << "selamat belajar di Ti umy" << endl;
+        throw 0,5; //melempar sebuah interger maka
+        cout << "pernyataan tidak akan di eksekusi" << endl;
+    }
+
+   
+}
