a764fbb343fc37f774de32e0ea32e849be12b7ad
miftahulafdhalzikri <miftahulafdhalzikri@gmail.com>
2025-06-10 15:31:21 +0700

    masukan nama file

---
+++ iofileWithPath.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string namaFile;
+
+    cout << "masukan nama file: ";
+    cin >> namaFile;
+
+
+
+  
+
+   
+}
